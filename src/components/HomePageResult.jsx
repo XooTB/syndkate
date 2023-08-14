757f497348b2f8dbd95e3ad1cacd8bfca7336550
Samiul Alim <xo.ot2001@gmail.com>
2023-08-14 16:30:26 +0600

    Updated Routing

--- src/components/HomePageResult.jsx
+++ src/components/HomePageResult.jsx
@@ -1,22 +1,31 @@
 import React from "react";
 import useStore from "../hooks/useStore";
+import { useNavigate } from "react-router-dom";
 
 const HomePageResult = ({ product }) => {
+  const navigate = useNavigate();
   const link = `/product/${product._id}`;
+
+  const handleNavigation = () => {
+    navigate(`/product/${product._id}`);
+  };
 
   return (
     <div className="bg-background1 bg-opacity-20 mt-1 flex border-[1px] px-3 py-2">
       <div className="w-1/5 md:w-1/6 flex items-center justify-center">
-        <a href={link}>
-          <img src={product.image} className="w-12 cursor-pointer" />
-        </a>
+        <img
+          src={product.image}
+          className="w-12 cursor-pointer"
+          onClick={handleNavigation}
+        />
       </div>
       <div className="pl-2 md:pl-0 text-text1 text-left font-noto w-4/6">
-        <a href={link}>
-          <h3 className=" text-xs md:text-lg hover:text-accent1">
-            {product.productTitle}
-          </h3>
-        </a>
+        <h3
+          className=" text-xs md:text-lg hover:text-accent1 cursor-pointer"
+          onClick={handleNavigation}
+        >
+          {product.productTitle}
+        </h3>
         <p className="text-xs">
           <span className="text-accent1">Price:</span> {product.latestPrice}
         </p>
--- src/components/ProductOverview.jsx
+++ src/components/ProductOverview.jsx
@@ -5,21 +5,25 @@ import { useNavigate } from "react-router-dom";
 const ProductOverview = ({ product }) => {
   const navigate = useNavigate();
 
-  const link = `/product/${product._id}`;
+  const handleNavigation = () => {
+    navigate(`/product/${product._id}`);
+  };
 
   return (
     <div className="flex justify-center items-center rounded-xl mt-2 px-3 py-2 text-text1 bg-background1 bg-opacity-50 font-opensans text-base md:text-lg hover:bg-opacity-60">
       <img
         src={product.image}
         className="h-20 p-2 md:w-28 bg-background border-[1px] border-background1 rounded-md hover:cursor-pointer"
+        onClick={handleNavigation}
       />
 
       <div className="flex flex-col pl-2 md:pl-4 pt-2 w-2/3">
-        <a href={link}>
-          <h1 className="font-inika text-sm md:text-lg hover:cursor-pointer hover:text-accent1">
-            {product.productTitle}
-          </h1>
-        </a>
+        <h1
+          className="font-inika text-sm md:text-lg hover:cursor-pointer hover:text-accent1"
+          onClick={handleNavigation}
+        >
+          {product.productTitle}
+        </h1>
         <h3 className="pt-3 text-sm font-semibold md:text-base">
           <span className="text-accent1 font-opensans">Price:</span>{" "}
           {product.latestPrice} BDT
